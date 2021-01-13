ae2e324dac2973994fc3d70140a3e334916b8976
Rob Sanderson <rob@preeminent.it>
2021-01-13 23:46:28 +0000

    Add depends on vault_pki_secret_backend_root_sign_intermediate.intermediate

--- jenkins.tf
+++ jenkins.tf
@@ -1,5 +1,5 @@
 resource "vault_approle_auth_backend_role" "jenkins" {
   backend        = vault_auth_backend.approle.path
   role_name      = "jenkins"
-  token_policies = ["default", "dev", "prod"]
+  token_policies = ["default", "infra"]
 }
--- pki.tf
+++ pki.tf
@@ -56,6 +56,7 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
 resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
   backend     = vault_mount.pki_intermediate_ca.path
   certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+  depends_on  = [vault_pki_secret_backend_root_sign_intermediate.intermediate]
 }
 
 // Generic Infra PKI
