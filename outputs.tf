682f7150cb0f68b8640b27bbd2254acede07f89b
Rob Sanderson <rob@preeminent.it>
2021-01-16 10:39:49 +0000

    Add url and crl config to pki

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "main" {
     lxd_storage_pool                                   = lxd_storage_pool.main
     vault_approle_auth_backend_role                    = merge(vault_approle_auth_backend_role.infra, vault_approle_auth_backend_role.jenkins)
     vault_auth_backend                                 = vault_auth_backend.approle
-    vault_mount                                        = merge(vault_mount.pki_infra_ca, vault_mount.pki_intermediate_ca, vault_mount.pki_root_ca, vault_mount.secrets_infra)
+    vault_mount                                        = merge(vault_mount.infra, vault_mount.intermediate, vault_mount.root, vault_mount.secrets_infra)
     vault_pki_secret_backend_intermediate_cert_request = merge(vault_pki_secret_backend_intermediate_cert_request.infra, vault_pki_secret_backend_intermediate_cert_request.intermediate)
     vault_pki_secret_backend_intermediate_set_signed   = merge(vault_pki_secret_backend_intermediate_set_signed.infra, vault_pki_secret_backend_intermediate_set_signed.intermediate)
     vault_pki_secret_backend_role                      = vault_pki_secret_backend_role.infra
--- pki.tf
+++ pki.tf
@@ -1,15 +1,15 @@
 // Root PKI
-resource "vault_mount" "pki_root_ca" {
+resource "vault_mount" "root" {
   description               = "PKI for Root CA"
   type                      = "pki"
   path                      = "pki-root-ca"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 86400
+  default_lease_ttl_seconds = var.vault_root_ca.ttl
+  max_lease_ttl_seconds     = var.vault_root_ca.ttl
   depends_on                = [lxd_container.vault]
 }
 
 resource "vault_pki_secret_backend_root_cert" "root" {
-  backend              = vault_mount.pki_root_ca.path
+  backend              = vault_mount.root.path
   type                 = var.vault_root_ca.type
   common_name          = var.vault_root_ca.common_name
   ttl                  = var.vault_root_ca.ttl
@@ -20,20 +20,34 @@ resource "vault_pki_secret_backend_root_cert" "root" {
   exclude_cn_from_sans = var.vault_root_ca.exclude_cn_from_sans
   ou                   = var.vault_root_ca.ou
   organization         = var.vault_root_ca.organization
-  depends_on           = [vault_mount.pki_root_ca]
+  depends_on           = [vault_mount.root]
+}
+
+resource "vault_pki_secret_backend_config_urls" "root" {
+  backend                 = vault_mount.root.path
+  issuing_certificates    = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.root.path}/ca"]
+  crl_distribution_points = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.root.path}/crl"]
+  depends_on              = [vault_pki_secret_backend_root_cert.root]
+}
+
+resource "vault_pki_secret_backend_crl_config" "root" {
+  backend    = vault_mount.root.path
+  disable    = true
+  expiry     = "72h"
+  depends_on = [vault_pki_secret_backend_root_cert.root, vault_pki_secret_backend_config_urls.root]
 }
 
 // Intermediate PKI
-resource "vault_mount" "pki_intermediate_ca" {
+resource "vault_mount" "intermediate" {
   description               = "PKI for Intermediate CA"
   type                      = "pki"
   path                      = "pki-intermediate-ca"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 86400
+  default_lease_ttl_seconds = var.vault_intermediate_ca.ttl
+  max_lease_ttl_seconds     = var.vault_intermediate_ca.ttl
 }
 
 resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
-  backend            = vault_mount.pki_intermediate_ca.path
+  backend            = vault_mount.intermediate.path
   type               = var.vault_intermediate_ca.type
   common_name        = var.vault_intermediate_ca.common_name
   format             = var.vault_intermediate_ca.format
@@ -44,7 +58,7 @@ resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
 }
 
 resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
-  backend              = vault_mount.pki_root_ca.path
+  backend              = vault_mount.root.path
   csr                  = vault_pki_secret_backend_intermediate_cert_request.intermediate.csr
   common_name          = var.vault_intermediate_ca.common_name
   ttl                  = var.vault_intermediate_ca.ttl
@@ -55,33 +69,47 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
 }
 
 resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
-  backend     = vault_mount.pki_intermediate_ca.path
+  backend     = vault_mount.intermediate.path
   certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
   depends_on  = [vault_pki_secret_backend_root_sign_intermediate.intermediate]
 }
 
+resource "vault_pki_secret_backend_config_urls" "intermediate" {
+  backend                 = vault_mount.intermediate.path
+  issuing_certificates    = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.intermediate.path}/ca"]
+  crl_distribution_points = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.intermediate.path}/crl"]
+  depends_on              = [vault_pki_secret_backend_intermediate_set_signed.intermediate]
+}
+
+resource "vault_pki_secret_backend_crl_config" "intermediate" {
+  backend    = vault_mount.intermediate.path
+  disable    = true
+  expiry     = "72h"
+  depends_on = [vault_pki_secret_backend_intermediate_set_signed.intermediate, vault_pki_secret_backend_config_urls.intermediate]
+}
+
 // Generic Infra PKI
-resource "vault_mount" "pki_infra_ca" {
+resource "vault_mount" "infra" {
   description               = "PKI for generic infrastructure"
   type                      = "pki"
   path                      = "pki-infra"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 86400
+  default_lease_ttl_seconds = var.vault_infra_ca.ttl
+  max_lease_ttl_seconds     = var.vault_infra_ca.ttl
 }
 
 resource "vault_pki_secret_backend_intermediate_cert_request" "infra" {
-  backend            = vault_mount.pki_infra_ca.path
+  backend            = vault_mount.infra.path
   type               = var.vault_infra_ca.type
   common_name        = var.vault_infra_ca.common_name
   format             = var.vault_infra_ca.format
   private_key_format = var.vault_infra_ca.private_key_format
   key_type           = var.vault_infra_ca.key_type
   key_bits           = var.vault_infra_ca.key_bits
-  depends_on         = [vault_mount.pki_intermediate_ca]
+  depends_on         = [vault_mount.intermediate]
 }
 
 resource "vault_pki_secret_backend_root_sign_intermediate" "infra" {
-  backend              = vault_mount.pki_intermediate_ca.path
+  backend              = vault_mount.intermediate.path
   csr                  = vault_pki_secret_backend_intermediate_cert_request.infra.csr
   common_name          = var.vault_infra_ca.common_name
   ttl                  = var.vault_infra_ca.ttl
@@ -92,12 +120,26 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "infra" {
 }
 
 resource "vault_pki_secret_backend_intermediate_set_signed" "infra" {
-  backend     = vault_mount.pki_infra_ca.path
+  backend     = vault_mount.infra.path
   certificate = vault_pki_secret_backend_root_sign_intermediate.infra.certificate
 }
 
+resource "vault_pki_secret_backend_config_urls" "infra" {
+  backend                 = vault_mount.infra.path
+  issuing_certificates    = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.infra.path}/ca"]
+  crl_distribution_points = ["https://vault.${var.domain_name}:8200/v1/${vault_mount.infra.path}/crl"]
+  depends_on              = [vault_pki_secret_backend_intermediate_set_signed.infra]
+}
+
+resource "vault_pki_secret_backend_crl_config" "infra" {
+  backend    = vault_mount.infra.path
+  disable    = true
+  expiry     = "72h"
+  depends_on = [vault_pki_secret_backend_intermediate_set_signed.infra, vault_pki_secret_backend_config_urls.infra]
+}
+
 resource "vault_pki_secret_backend_role" "infra" {
-  backend           = vault_mount.pki_infra_ca.path
+  backend           = vault_mount.infra.path
   name              = "infra"
   allow_localhost   = false
   allowed_domains   = [var.domain_name]
